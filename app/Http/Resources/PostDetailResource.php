e71996a63d9e86448d44d53f4c3568867b795c83
mustajab15 <mustajabamir151@gmail.com>
2024-05-27 19:49:48 +0800

    Api Resource Commit

---
+++ app/Http/Resources/PostDetailResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'news_content' => $this->news_content,
+            'author' => $this->author,
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d'),
+            'writer' => $this->whenLoaded('writer')
+
+        ];
+    }
+}
