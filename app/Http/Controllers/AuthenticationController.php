143a67f1e4ce865c79e751e01405b63bc4cd2c76
mustajab15 <mustajabamir151@gmail.com>
2024-05-28 15:25:50 +0800

    Authentication API Token Commit

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Validation\ValidationException;
+
+class AuthenticationController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        return $user->createToken('user login')->plainTextToken;
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+    }
+
+    public function me(Request $request)
+    {
+        return response()->Json(Auth::user());
+    }
+}
